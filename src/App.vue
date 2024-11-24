88182195d236e32b6da14672c44f938a23bc2f3b
mqnu00 <647983952@qq.com>
2024-11-24 16:46:39 +0800

    feat: echarts

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+  <SysInfo></SysInfo>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from './components/HelloWorld.vue';
+import SysInfo from './components/SysInfo.vue'
 
 @Options({
   components: {
     HelloWorld,
+    SysInfo
   },
 })
 export default class App extends Vue {}
