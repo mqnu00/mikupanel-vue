a379f483d8e0566095559b42adc5cec830eed6b9
heixiaosun <heixiaosun@i-i.ai>
2024-11-25 15:40:14 +0800

    feat: 修改按钮样式

--- src/components/SysInfo.vue
+++ src/components/SysInfo.vue
@@ -1,16 +1,31 @@
 <template>
     <!-- 为ECharts准备一个具备大小（宽高）的Dom -->
     <div class="chart-with-button">
-        <div id="cpuChart" class="chart-container">
+        <div>
+            <div id="cpuChart" class="chart-container">
+            </div>
+
+            <!-- 切换图表的按钮组 -->
+            <div class="cpu">
+                <button class="toggle-button" 
+                :class="{ 'active': radio1 === '流量' }"
+                @click="check('流量')"
+                >
+                    流量
+                </button>
+                <button class="toggle-button" 
+                :class="{ 'active': radio1 === '磁盘IO' }"
+                @click="check('磁盘IO')">
+                    磁盘IO
+                </button>
+                <button class="toggle-button"
+                :class="{ 'active': radio1 === '测试' }"
+                @click="check('测试')">
+                测试
+                </button>
+            </div>
+        </div>
     </div>
-    </div>
-        <!-- 切换图表的按钮组 -->
-        <el-radio-group v-model="radio1" size="large">
-            <el-radio-button label="New York" value="New York" />
-            <el-radio-button label="Washington" value="Washington" />
-            <el-radio-button label="Los Angeles" value="Los Angeles" />
-            <el-radio-button label="Chicago" value="Chicago" />
-        </el-radio-group>
 </template>
 
 <script lang="ts">
@@ -21,17 +36,20 @@ import { defineComponent, onMounted, onUnmounted, ref } from "vue";
 //引入创建的echarts.ts文件
 import echarts from "../utils/echarts";
 import { getCpuUsage } from "../client/sysInfo";
+import { number } from "echarts";
 
 export default defineComponent({
-    
+
     methods: {
+        check(name: string){
+            this.radio1=name
+            console.log(this.radio1)
+        }
     },
 
     setup() {
 
         let socket: WebSocket | null = null;
-        const cpuinfo: number[] = [];
-        const cputime: number[] = [];
         /**
          * 在使用init方法初始化图表之前，确保DOM元素已经被正确加载。在Vue组件中，
          * 可以使用onMounted钩子函数来确保在DOM准备就绪后再执行初始化操作。
@@ -86,7 +104,7 @@ export default defineComponent({
                 xAxis: {
                     type: "category",
                     boundaryGap: false,
-                    data: cputime,
+                    data: [] as number[],
                     name: '/s',
                 },
                 yAxis: {
@@ -107,7 +125,7 @@ export default defineComponent({
                         focus: "series",
                     },
                     symbol: 'none',
-                    data: cpuinfo
+                    data: [] as number[]
                 },
 
             };
@@ -117,15 +135,13 @@ export default defineComponent({
                 let i = 0
 
                 socket = getCpuUsage((data) => {
-                    cpuinfo.push(parseFloat(data))
+                    option.series.data.push(parseFloat(data))
                     if (i != 60) {
                         i = i + 1
-                        cputime.push(i)
+                        option.xAxis.data.push(i)
                     } else {
-                        cpuinfo.splice(0, 1)
+                        option.series.data.splice(0, 1)
                     }
-                    option.xAxis.data = cputime
-                    option.series.data = cpuinfo
                     myChart.setOption(option, true);
                 })
             } catch (error) {
@@ -146,17 +162,18 @@ export default defineComponent({
 
         return {
             cputime: '',
-            radio1: ref("New York"),
+            radio1: ref("流量"),
         };
     },
 });
 </script>
 
 <style>
-
 .chart-with-button {
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
     /* 轻微的阴影效果 */
+    padding: 20px;
+    /* 为按钮组留出空间 */
 }
 
 /* 图表容器样式 */
@@ -170,6 +187,43 @@ export default defineComponent({
     position: relative;
     /* 设置相对定位，以便按钮组可以绝对定位 */
 }
+
+/* 为radio组容器设置绝对定位 */
+.cpu {
+    position: absolute;
+    top: 10px;
+    /* 距离顶部10px */
+    right: 20px;
+    /* 距离右侧10px */
+}
+
+/* 按钮样式 */
+.toggle-button {
+  padding: 5px 10px; /* 内边距 */
+  width: 100px;
+  height: 50px;
+  font-size: 14px; /* 字体大小 */
+  cursor: pointer; /* 鼠标悬停时显示指针 */
+  background: none; /* 背景颜色 */
+  color: rgb(0, 0, 0); /* 文字颜色 */
+  border: none; /* 无边框 */
+  outline: none; /* 点击时无轮廓 */
+  margin-right: 0;
+  transition: background-color 0.3s; /* 背景颜色变化的过渡效果 */
+}
+
+/* 按钮悬停时的样式 */
+.toggle-button:hover {
+  background-color: #d3d8f6; /* 悬停时的背景颜色 */
+  border-bottom: 2px solid #4e77ea; /* 添加底部边框 */
+}
+
+.toggle-button.active {
+    background-color: #b3bdf2; /* 悬停时的背景颜色 */
+    border-bottom: 2px solid #4e77ea; /* 添加底部边框 */
+}
 </style>
+
+<style lang="less" scoped></style>
 
 <style scoped></style>
