9961fcb00bd7ae1c634d965d8a5810f065eec5ee
heixiaosun <heixiaosun@i-i.ai>
2024-11-25 17:47:18 +0800

    feat: 设置为图表按钮网格组

--- src/components/SysInfo.vue
+++ src/components/SysInfo.vue
@@ -1,12 +1,23 @@
 <template>
     <!-- 为ECharts准备一个具备大小（宽高）的Dom -->
-    <div class="chart-with-button">
-        <div>
-            <div id="cpuChart" class="chart-container">
+    <div class="tot">
+        <div class="chart-with-button" v-for="(group, index) in buttonGroups" :key="'group-' + index">
+
+
+
+
+            <div class="cpu">
+                <button v-for="item in group.buttons" :key="item.id" class="toggle-button">{{ item.name }}
+                </button>
             </div>
 
-            <!-- 切换图表的按钮组 -->
-            <div class="cpu">
+            <div :id="group.chart" class="chart-container"></div>
+
+        </div>
+
+    </div>
+    <!-- 切换图表的按钮组 -->
+    <!-- <div class="cpu"></div>
                 <button class="toggle-button" 
                 :class="{ 'active': radio1 === '流量' }"
                 @click="check('流量')"
@@ -23,9 +34,7 @@
                 @click="check('测试')">
                 测试
                 </button>
-            </div>
-        </div>
-    </div>
+            </div> -->
 </template>
 
 <script lang="ts">
@@ -41,8 +50,8 @@ import { number } from "echarts";
 export default defineComponent({
 
     methods: {
-        check(name: string){
-            this.radio1=name
+        check(name: string) {
+            this.radio1 = name
             console.log(this.radio1)
         }
     },
@@ -163,28 +172,102 @@ export default defineComponent({
         return {
             cputime: '',
             radio1: ref("流量"),
+            buttonGroups: [
+                {
+                    buttons: [
+                        {
+                            id: 1,
+                            name: '1'
+                        },
+                        {
+                            id: 1,
+                            name: '1'
+                        }
+                    ],
+                    chart: 'cpuChart'
+                },
+                {
+                    buttons: [
+                        {
+                            id: 1,
+                            name: '1'
+                        },
+                        {
+                            id: 1,
+                            name: '1'
+                        }
+                    ],
+                    chart: 'cpuChart2'
+                },
+                {
+                    buttons: [
+                        {
+                            id: 1,
+                            name: '1'
+                        },
+                        {
+                            id: 1,
+                            name: '1'
+                        }
+                    ],
+                    chart: 'cpuChart3'
+                },
+                {
+                    buttons: [
+                        {
+                            id: 1,
+                            name: '1'
+                        },
+                        {
+                            id: 1,
+                            name: '1'
+                        }
+                    ],
+                    chart: 'cpuChart4'
+                },
+            ]
         };
     },
 });
 </script>
 
-<style>
+<style scoped>
+.tot {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    /* 两列布局，每列占屏幕宽度的50% */
+    gap: 20px;
+    /* 组件之间的间隙 */
+    width: 95%;
+    /* 容器宽度占满整个视口宽度 */
+    height: 95%;
+    /* 容器高度占满整个视口高度 */
+    place-items: center;
+    /* 使内容在容器中居中 */
+}
+
 .chart-with-button {
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
     /* 轻微的阴影效果 */
+    gap: 1px;
+    /* 按钮之间的间隙 */
+    width: 90%;
+    /* 按钮组宽度占满容器宽度 */
+    height: 90%;
     padding: 20px;
-    /* 为按钮组留出空间 */
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    position: relative;
 }
 
 /* 图表容器样式 */
 .chart-container {
-    width: 800px;
+    width: 100%;
     /* 图表宽度 */
     height: 400px;
     /* 图表高度 */
     margin: 20px auto;
     /* 外边距，居中显示 */
-    position: relative;
+    /* position: relative; */
     /* 设置相对定位，以便按钮组可以绝对定位 */
 }
 
@@ -199,31 +282,41 @@ export default defineComponent({
 
 /* 按钮样式 */
 .toggle-button {
-  padding: 5px 10px; /* 内边距 */
-  width: 100px;
-  height: 50px;
-  font-size: 14px; /* 字体大小 */
-  cursor: pointer; /* 鼠标悬停时显示指针 */
-  background: none; /* 背景颜色 */
-  color: rgb(0, 0, 0); /* 文字颜色 */
-  border: none; /* 无边框 */
-  outline: none; /* 点击时无轮廓 */
-  margin-right: 0;
-  transition: background-color 0.3s; /* 背景颜色变化的过渡效果 */
+    padding: 5px 10px;
+    /* 内边距 */
+    width: 100px;
+    height: 50px;
+    font-size: 14px;
+    /* 字体大小 */
+    cursor: pointer;
+    /* 鼠标悬停时显示指针 */
+    background: none;
+    /* 背景颜色 */
+    color: rgb(0, 0, 0);
+    /* 文字颜色 */
+    border: none;
+    /* 无边框 */
+    outline: none;
+    /* 点击时无轮廓 */
+    margin-right: 0;
+    transition: background-color 0.3s;
+    /* 背景颜色变化的过渡效果 */
 }
 
 /* 按钮悬停时的样式 */
 .toggle-button:hover {
-  background-color: #d3d8f6; /* 悬停时的背景颜色 */
-  border-bottom: 2px solid #4e77ea; /* 添加底部边框 */
+    background-color: #d3d8f6;
+    /* 悬停时的背景颜色 */
+    border-bottom: 2px solid #4e77ea;
+    /* 添加底部边框 */
 }
 
 .toggle-button.active {
-    background-color: #b3bdf2; /* 悬停时的背景颜色 */
-    border-bottom: 2px solid #4e77ea; /* 添加底部边框 */
+    background-color: #b3bdf2;
+    /* 悬停时的背景颜色 */
+    border-bottom: 2px solid #4e77ea;
+    /* 添加底部边框 */
 }
 </style>
-
-<style lang="less" scoped></style>
 
 <style scoped></style>
