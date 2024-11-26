261bf048a679efe756eebddd0321d20f91bf6bed
mikufan <mqnu000@gmail.com>
2024-11-26 17:20:34 +0800

    feat: memory socket

--- src/client/sysInfo.ts
+++ src/client/sysInfo.ts
@@ -36,7 +36,7 @@ export function getCpuUsage(onMessage: (data: string) => void): WebSocket {
 
     // 收到新消息时，调用传入的回调函数
     socket.onmessage = (event) => {
-        console.log("接收到消息：" + event.data);
+        // console.log("接收到消息：" + event.data);
         onMessage(event.data.toString()); // 将消息传递给调用方
     };
 
@@ -51,4 +51,32 @@ export function getCpuUsage(onMessage: (data: string) => void): WebSocket {
     };
 
     return socket;
+}
+
+export function getMemoryUsage(onMessage: (data: string) => void): WebSocket {
+    
+  const socket = new WebSocket("ws://localhost:8000/memory");
+
+  // WebSocket 打开时
+  socket.onopen = () => {
+      console.log("WebSocket 连接已建立");
+  };
+
+  // 收到新消息时，调用传入的回调函数
+  socket.onmessage = (event) => {
+      // console.log("接收到消息：" + event.data);
+      onMessage(event.data.toString()); // 将消息传递给调用方
+  };
+
+  // 发生错误时
+  socket.onerror = (error) => {
+      console.error("WebSocket 发生错误：", error);
+  };
+
+  // 连接关闭时
+  socket.onclose = () => {
+      console.log("WebSocket 连接已关闭");
+  };
+
+  return socket;
 }
