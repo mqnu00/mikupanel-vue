ef8825fc9d9c76738ffb0c2717de249f0429d502
mikufan <mqnu000@gmail.com>
2024-11-28 18:14:52 +0800

    feat: network socket

--- src/client/sysInfo.ts
+++ src/client/sysInfo.ts
@@ -80,3 +80,31 @@ export function getMemoryUsage(onMessage: (data: string) => void): WebSocket {
 
   return socket;
 }
+
+export function getNetworkUsage(onMessage: (data: string) => void): WebSocket {
+    
+  const socket = new WebSocket("ws://localhost:8000/network");
+
+  // WebSocket 打开时
+  socket.onopen = () => {
+      console.log("WebSocket 连接已建立");
+  };
+
+  // 收到新消息时，调用传入的回调函数
+  socket.onmessage = (event) => {
+      // console.log("接收到消息：" + event.data);
+      onMessage(event.data.toString()); // 将消息传递给调用方
+  };
+
+  // 发生错误时
+  socket.onerror = (error) => {
+      console.error("WebSocket 发生错误：", error);
+  };
+
+  // 连接关闭时
+  socket.onclose = () => {
+      console.log("WebSocket 连接已关闭");
+  };
+
+  return socket;
+}
