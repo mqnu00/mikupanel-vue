d3af69a575b3776f2dd3bc5c33d66839b7b52f4d
heixiaosun <heixiaosun@i-i.ai>
2024-12-12 16:51:36 +0800

    feat: scss

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'element-plus/dist/index.css';
+import './assets/global.scss'
 
 const app = createApp(App)
 
