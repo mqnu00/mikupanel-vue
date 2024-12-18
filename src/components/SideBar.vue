78c062dcc9610ba850f826d079bccffb8d9bc6cd
heixiaosun <heixiaosun@i-i.ai>
2024-12-18 18:26:51 +0800

    feat: terminal

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,81 +1,86 @@
 <template>
-  <el-menu
-    class="sidebar"
-    default-active="/"
-    background-color="#001529"
-    text-color="#fff"
-    active-text-color="#409EFF"
-    router
-  >
-    <!-- 概览 -->
-    <el-menu-item index="/">
-      <el-icon style="vertical-align: middle">
-        <House />
-      </el-icon>
-      <span>概览</span>
-    </el-menu-item>
+    <el-menu class="sidebar" default-active="/" background-color="#001529" text-color="#fff" active-text-color="#409EFF"
+        router>
+        <!-- 概览 -->
+        <el-menu-item index="/">
+            <el-icon style="vertical-align: middle">
+                <House />
+            </el-icon>
+            <span>概览</span>
+        </el-menu-item>
 
-    <!-- 应用商店 -->
-    <el-menu-item index="/app-store">
-      <el-icon style="vertical-align: middle">
-        <Shop />
-      </el-icon>
-      <span>应用商店</span>
-    </el-menu-item>
+        <!-- 应用商店 -->
+        <el-menu-item index="/app-store">
+            <el-icon style="vertical-align: middle">
+                <Shop />
+            </el-icon>
+            <span>应用商店</span>
+        </el-menu-item>
 
-    <!-- 网站 -->
-    <el-sub-menu index="3">
-      <template #title>
-        <el-icon style="vertical-align: middle">
-          <Grid />
-        </el-icon>
-        <span>网站</span>
-      </template>
-      <el-menu-item index="/website/option1">选项 1</el-menu-item>
-      <el-menu-item index="/website/option2">选项 2</el-menu-item>
-    </el-sub-menu>
-  </el-menu>
+        <!-- 终端 -->
+        <el-menu-item index="/terminal">
+            <el-icon style="vertical-align: middle">
+                <Shop />
+            </el-icon>
+            <span>终端</span>
+        </el-menu-item>
+
+        <!-- 网站 -->
+        <el-sub-menu index="3">
+            <template #title>
+                <el-icon style="vertical-align: middle">
+                    <Grid />
+                </el-icon>
+                <span>网站</span>
+            </template>
+            <el-menu-item index="/website/option1">选项 1</el-menu-item>
+            <el-menu-item index="/website/option2">选项 2</el-menu-item>
+        </el-sub-menu>
+    </el-menu>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted } from 'vue';
 import { House, Grid } from '@element-plus/icons-vue';
 
 export default defineComponent({
-  name: 'SideBar',
-  components: {
-    House,
-    Grid,
-  },
+    name: 'SideBar',
+    components: {
+        House,
+        Grid,
+    }
 });
 </script>
 
 <style scoped>
-
-
 .sidebar {
-  height: 100vh;
-  width: 220px;
-  background-color: #001529;
-  color: white;
+    height: 100vh;
+    width: 220px;
+    background-color: #001529;
+    color: white;
 }
 
 .el-icon {
-  font-size: 16px; /* 控制图标大小 */
-  margin-right: 10px; /* 图标和文字间距 */
+    font-size: 16px;
+    /* 控制图标大小 */
+    margin-right: 10px;
+    /* 图标和文字间距 */
 }
 
-.el-menu-item.is-active > .el-icon {
-  color: #409EFF; /* 选中状态的
+.el-menu-item.is-active>.el-icon {
+    color: #409EFF;
+    /* 选中状态的
   图标颜色 */
 }
 
 /* Active menu item style */
 .el-menu-item.is-active {
-  background-color: rgba(64, 158, 255, 0.2) !important; /* Light blue background */
-  border-left: 4px solid #409EFF !important; /* Blue left border */
-  color: #409EFF !important; /* Text color */
-  font-weight: bold;
+    background-color: rgba(64, 158, 255, 0.2) !important;
+    /* Light blue background */
+    border-left: 4px solid #409EFF !important;
+    /* Blue left border */
+    color: #409EFF !important;
+    /* Text color */
+    font-weight: bold;
 }
-
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -15,6 +15,11 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../components/SysInfo.vue')
+  },
+  {
+    path: '/terminal',
+    name: 'terminal',
+    component: () => import(/* webpackChunkName: "about" */ '../components/TerminalShell.vue')
   }
 ]
 
