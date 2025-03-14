8991f42a6ad7d60e9beb58b735946dc082c3f409
mqnu00 <647983952@qq.com>
2025-03-14 05:27:12 -0400

    chore: 自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,40 @@ import { resolve } from 'path';
 import Components from 'unplugin-vue-components/vite';
 import { NaiveUiResolver, ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
+import AutoImport from "unplugin-auto-import/vite";
+import Icons from "unplugin-icons/vite";
+import IconsResolver from "unplugin-icons/resolver";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      imports: ['vue'],
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({
+          prefix: "Icon",
+        }),
+      ],
+    }),
     Components({
       resolvers: [
         NaiveUiResolver(),
-        ElementPlusResolver()
+        ElementPlusResolver(),
+        IconsResolver(
+          {
+            enabledCollections: ["ep"],
+          }
+        )
       ],
       dts: 'src/types/components.d.ts', // 生成类型声明文件
+      
     }),
+    Icons({
+      autoInstall: true,
+      compiler: 'vue3'
+    }),
+
   ],
   resolve: {
     alias: {
