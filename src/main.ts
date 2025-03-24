0f1b59fea7ce5255c83a4fa3037af47789557c87
mqnu00 <647983952@qq.com>
2025-03-24 04:02:09 -0400

    feat: 全局缓存

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 import {create as createNaiveUI} from 'naive-ui'
 import { LogoAppleAppstore } from '@vicons/ionicons5'
 
 const app = createApp(App)
+const pinia = createPinia()
 // const naive = createNaiveUI()
-app.use(router)
+app
+.use(router)
+.use(pinia)
 // app.use(naive)
 app.mount('#app')
